2f779cba5db062d1f09b2597dd677ef552d9bf9f
ViniciusCampeao <viniciusfernandes643@gmail.com>
2024-11-18 19:48:28 -0300

    fix: small adjustments

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -2,9 +2,9 @@ import Bruna from "../../assets/images/Bruna.png";
 
 const Hero = () => { 
   return (
-    <div className="flex flex-col md:flex-row justify-between">
+    <div className="flex flex-col md:flex-row justify-between items-center">
       <div className="md:w-1/2 w-full px-12  md:py-52 text-center">
-        <h1 className=" font-quicksand text-xl font md:text-3xl mb-10 text-center font-semibold">
+        <h1 className=" font-quicksand text-xl font md:text-3xl mb-10 text-center font-semibold mt-10">
         Cada som é uma história esperando para ser contada!
         </h1>
         <p className="text-sm md:text-lg mb-10 font-quicksand text-center">
@@ -12,12 +12,12 @@ const Hero = () => {
         </p>
         <a
           className="inline-block rounded-full px-6 py-3 bg-blue-300 text-white hover:bg-blue-400 transition-all duration-300"
-          href="/about"
+          href="/contact"
         >
-          Saiba mais
+          Contato
         </a>
       </div>
-      <div className="md:w-1/2 items-end flex">
+      <div className="md:w-1/2 flex flex-col items-center w-[70%]">
         <img src={Bruna} alt="Bruna" />
       </div>
     </div>
